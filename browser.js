7abb4dbf2de24426b3e58ec3ab6605856277d67e
Silve <silverice.yuriy@gmail.com>
2019-02-07 02:28:16 +0300

    BUMP

---
+++ browser.js
@@ -0,0 +1,43 @@
+'use strict';
+const electron = require('electron');
+
+const ipc = electron.ipcRenderer;
+const webFrame = electron.webFrame;
+const config = require('./config');
+
+const el = config.get('element');
+
+function exec(command) {
+	webFrame.executeJavaScript(`if (!window.a) a = new Mu.Adapter(); ${command};`);
+}
+
+function click(s) {
+	const e = document.querySelector(s);
+	if (e) {
+		e.click();
+	}
+}
+
+ipc.on('preferences', () => {
+	click(el.prefButton);
+	window.setTimeout(() => {
+		let w = document.documentElement.scrollWidth / 2 | 0;
+		let h = document.documentElement.scrollHeight / 2 | 0;
+		let pref = document.querySelector(el.prefDialog);
+		let pw = pref.offsetWidth / 2 | 0;
+		let ph = pref.offsetHeight / 2 | 0;
+		pref.style.top = `${h - ph}px`;
+		pref.style.left = `${w - pw}px`;
+	}, 25);
+});
+
+ipc.on('log-out', () => {
+
+});
+
+ipc.on('play', () => exec('a.togglePause()'));
+ipc.on('next', () => exec('a.next()'));
+ipc.on('like', () => click(el.like));
+ipc.on('dislike', () => click(el.dislike));
+ipc.on('mute', () => exec('a.mute()'));
+ipc.on('HQ', () => exec('a.toggleHQ()'));
